2e1a1655e327407e375534a1ed0b2cfadb7b80d1
DinushiJayasekara <jkdrj02@gmail.com>
2020-11-06 19:23:17 +0530

    extract-stateless-quote-card

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'quote.dart';
+import 'quote_card.dart';
 
 void main() {
   runApp(MyApp());
@@ -37,30 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
   ];
 
   Widget quoteTemplate(quote) {
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(quote.text,
-                style: TextStyle(
-                  fontSize: 18.0,
-                  color: Colors.grey[600],
-                )),
-            SizedBox(height: 6.0),
-            Text(
-              quote.author,
-              style: TextStyle(
-                fontSize: 14.0,
-                color: Colors.grey[700],
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+    return QuoteCard(quote: quote);
   }
 
   @override
